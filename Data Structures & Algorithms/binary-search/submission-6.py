25401779ec97f9e58a7acfbdd92eccfc8b068a50
Blank45-cmd <yashtope7@gmail.com>
2026-07-27 21:52:19 +0530

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        def binary(l, r):
+            if l > r:
+                return -1
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                return binary(l, m - 1)
+            else:
+                return binary(m + 1, r)
+        return binary(0, len(nums) - 1)
